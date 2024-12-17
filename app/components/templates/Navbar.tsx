4d0b73d0562704bdeffcc3fe9efc92675e3d1d54
bilal <abii.manyun@gmail.com>
2024-12-18 04:29:43 +0700

    fix navbar

--- app/components/templates/Navbar.tsx
+++ app/components/templates/Navbar.tsx
@@ -25,21 +25,23 @@ const Navbar = () => {
   }, [lastScrollY]);
 
   return (
-    <nav className={`font-medium fixed bottom-5 left-1/2 transform -translate-x-1/2 w-[95%] md:w-[720px] bg-gray-800 text-white py-2 shadow-lg z-50 rounded-full transition-all ${showNavbar ? "translate-y-0" : "translate-y-full"}`}>
-      <div className="flex justify-around space-x-6 p-3">
-        <Link href="/#home" className="hover:text-gray-400 transition-colors text-sm md:text-base">
+    <nav
+      className={`font-medium fixed bottom-5 left-1/2 transform -translate-x-1/2 w-[88%] sm:w-[92%] md:w-[720px] bg-gray-800 text-white py-2 shadow-lg z-50 rounded-full transition-all ${showNavbar ? "translate-y-0" : "translate-y-full"}`}
+    >
+      <div className="flex justify-center sm:justify-evenly space-x-3 sm:space-x-4 md:space-x-6 p-3 text-xs sm:text-sm md:text-base">
+        <Link href="/#home" className="hover:text-gray-400 transition-colors">
           Home
         </Link>
-        <Link href="/#about" className="hover:text-gray-400 transition-colors text-sm md:text-base">
+        <Link href="/#about" className="hover:text-gray-400 transition-colors">
           About
         </Link>
-        <Link href="/#experience" className="hover:text-gray-400 transition-colors text-sm md:text-base">
+        <Link href="/#experience" className="hover:text-gray-400 transition-colors">
           Experience
         </Link>
-        <Link href="/#projects" className="hover:text-gray-400 transition-colors text-sm md:text-base">
+        <Link href="/#projects" className="hover:text-gray-400 transition-colors">
           Projects
         </Link>
-        <Link href="/#contact" className="hover:text-gray-400 transition-colors text-sm md:text-base">
+        <Link href="/#contact" className="hover:text-gray-400 transition-colors">
           Contact
         </Link>
       </div>
